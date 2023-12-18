60fdccab3d5ea076c2a35a2439c6efd2a659974a
Muhizi Ngoga Bonheur <muhizibonheur8@gmail.com>
2023-12-18 12:57:09 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+putchar(letter);
+}
+
+putchar('\n'); /* Print a new line */
+
+return (0);
+}
+
